afbd510bf4830afbfba40dc79ded3ff38fd771f5
Bruno Nascimento <bn.id07@gmail.com>
2026-07-23 22:29:21 -0300

    feat: adicionadas classes de security

---
+++ src/main/java/com/nascimentodev/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,23 @@
+package com.nascimentodev.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "telefone")
+public class Telefone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "telefone", length = 10)
+    private String telefone;
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+}
